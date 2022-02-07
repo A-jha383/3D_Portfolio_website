250bc8e893618786d4e40a7c2dc7091f8396ae01
AYUSH JHA <ayushj383@gmail.com>
2022-02-07 23:38:50 +0530

    stars

--- bundler/webpack.common.js
+++ bundler/webpack.common.js
@@ -77,6 +77,15 @@ module.exports = {
           "sass-loader",
         ],
       },
+
+      // Shaders
+      {
+        test: /\.(glsl|vs|fs|vert|frag)$/,
+        exclude: /node_modules/,
+        use: [
+            'raw-loader'
+        ]
+    }
     ],
   },
 };
--- src/script.js
+++ src/script.js
@@ -1,251 +1,211 @@
 import "./CSS/style.css";
 import "./CSS/normalize.css";
-
-import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-
-const scene = new THREE.Scene();
-
-// Sizes
-const sizes = {
-  width: innerWidth,
-  height: innerHeight,
-};
-
-const target = new THREE.Vector2();
-const mouse = new THREE.Vector2();
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'dat.gui'
+import galaxyVertexShader from './shaders/galaxy/vertex.glsl'
+import galaxyFragmentShader from './shaders/galaxy/fragment.glsl'
 
 /**
- * Objects
+ * Base
  */
-const fontLoader = new THREE.FontLoader();
-fontLoader.load("/fonts/Poppins-SemiBold.json", (font) => {
-  const material = new THREE.MeshNormalMaterial();
-  const sentence = "creative developer awesome designs";
-  const data = {
-    words: sentence.split(" "),
-    positions: [
-      1.5 / 2 + 0.4 + 0.8 + 1.5,
-      1.5 / 2 + 0.4,
-      -(1.5 / 2 + 0.4),
-      -(1.5 / 2 + 0.4 + 0.8 + 1.5),
-    ],
-  };
-  let mesh;
-  for (let i = 0; i < data.words.length; i++) {
-    const textGeometry = new THREE.TextBufferGeometry(data.words[i], {
-      font: font,
-      size: 1.5,
-      height: 1.8,
-      curveSegments: 12,
-      bevelEnabled: true,
-      bevelSize: 0.05,
-      bevelThickness: 0.03,
-      bevelOffset: 0,
-      bevelSegments: 5,
-    });
-    textGeometry.center();
-    mesh = new THREE.Mesh(textGeometry, material);
-    mesh.position.y = data.positions[i];
+// // Debug
+// const gui = new dat.GUI()
 
-    scene.add(mesh);
-  }
-  animate();
-});
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
 
-// Rings and boxes
-const params = {};
-params.count = 500;
-params.size = 88;
-params.rotation = 10;
+// Scene
+const scene = new THREE.Scene()
 
-let ringGeometry = null;
-let cubeGeometry = null;
-let Material = null;
-let ringMesh = null;
-let cubeMesh = null;
+const hemisphereLight = new THREE.HemisphereLight(0xff0000,0x0000ff,0.5)
+scene.add(hemisphereLight)
 
-let objectGroup = null;
+const fog = new THREE.Fog('#262837',1,15)
+scene.fog = fog
 
-function addObjects() {
-  if (objectGroup !== null) {
-    ringGeometry.dispose();
-    cubeGeometry.dispose();
 
-    Material.dispose();
-    scene.remove(objectGroup);
-  }
-  objectGroup = new THREE.Group();
+/**
+ * Galaxy
+ */
+const parameters = {}
+parameters.count = 200000
+parameters.size = 0.005
+parameters.radius = 5
+parameters.branches = 3
+parameters.spin = 1
+parameters.randomness = 0.2
+parameters.randomnessPower = 3
+parameters.insideColor = '#ff6030'
+parameters.outsideColor = '#1b3984'
 
-  ringGeometry = new THREE.TorusBufferGeometry(0.3, 0.17, 16, 120);
-  cubeGeometry = new THREE.BoxBufferGeometry(0.6, 0.6, 0.6);
+let geometry = null
+let material = null
+let points = null
 
-  Material = new THREE.MeshNormalMaterial();
+const generateGalaxy = () =>
+{
+    if(points !== null)
+    {
+        geometry.dispose()
+        material.dispose()
+        scene.remove(points)
+    }
 
-  for (let i = 0; i < params.count; i++) {
-    ringMesh = new THREE.Mesh(ringGeometry, Material);
-    ringMesh.position.x = (Math.random() - 0.5) * params.size;
-    ringMesh.position.y = (Math.random() - 0.5) * params.size;
-    ringMesh.position.z = (Math.random() - 0.5) * params.size;
+    /**
+     * Geometry
+     */
+    geometry = new THREE.BufferGeometry()
 
-    ringMesh.rotation.z = Math.sin(Math.random() * params.rotation);
-    ringMesh.rotation.y = Math.cos(Math.random() * params.rotation);
-    ringMesh.rotation.x = Math.sin(Math.random() * params.rotation);
-    objectGroup.add(ringMesh);
+    const positions = new Float32Array(parameters.count * 3)
+    const randomness = new Float32Array(parameters.count * 3)
+    const colors = new Float32Array(parameters.count * 3)
+    const scales = new Float32Array(parameters.count * 1)
 
-    cubeMesh = new THREE.Mesh(cubeGeometry, Material);
-    cubeMesh.position.x = (Math.random() - 0.5) * params.size;
-    cubeMesh.position.y = (Math.random() - 0.5) * params.size;
-    cubeMesh.position.z = (Math.random() - 0.5) * params.size;
+    const insideColor = new THREE.Color(parameters.insideColor)
+    const outsideColor = new THREE.Color(parameters.outsideColor)
 
-    cubeMesh.rotation.z = Math.sin(Math.random() * params.rotation);
-    cubeMesh.rotation.y = Math.cos(Math.random() * params.rotation);
-    objectGroup.add(cubeMesh);
-  }
-  scene.add(objectGroup);
+    for(let i = 0; i < parameters.count; i++)
+    {
+        const i3 = i * 3
+
+        // Position
+        const radius = Math.random() * parameters.radius
+
+        const branchAngle = (i % parameters.branches) / parameters.branches * Math.PI * 2
+
+        const randomX = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : - 1) * parameters.randomness * radius
+        const randomY = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : - 1) * parameters.randomness * radius
+        const randomZ = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : - 1) * parameters.randomness * radius
+
+        positions[i3    ] = Math.cos(branchAngle) * radius
+        positions[i3 + 1] = 0
+        positions[i3 + 2] = Math.sin(branchAngle) * radius
+    
+        randomness[i3    ] = randomX
+        randomness[i3 + 1] = randomY
+        randomness[i3 + 2] = randomZ
+
+        // Color
+        const mixedColor = insideColor.clone()
+        mixedColor.lerp(outsideColor, radius / parameters.radius)
+
+        colors[i3    ] = mixedColor.r
+        colors[i3 + 1] = mixedColor.g
+        colors[i3 + 2] = mixedColor.b
+
+        // Scale
+        scales[i] = Math.random()
+    }
+
+    geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+    geometry.setAttribute('aRandomness', new THREE.BufferAttribute(randomness, 3))
+    geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3))
+    geometry.setAttribute('aScale', new THREE.BufferAttribute(scales, 1))
+
+    /**
+     * Material
+     */
+    material = new THREE.ShaderMaterial({
+        depthWrite: false,
+        blending: THREE.AdditiveBlending,
+        vertexColors: true,
+        uniforms:
+        {
+            uTime: { value: 0 },
+            uSize: { value: 30 * renderer.getPixelRatio() }
+        },    
+        vertexShader: galaxyVertexShader,
+        fragmentShader: galaxyFragmentShader
+    })
+
+    /**
+     * Points
+     */
+    points = new THREE.Points(geometry, material)
+    scene.add(points)
 }
-addObjects();
 
-// Particles
-const particleGeometry = new THREE.BufferGeometry();
-const particleCount = 800;
+// gui.add(parameters, 'count').min(100).max(1000000).step(100).onFinishChange(generateGalaxy)
+// gui.add(parameters, 'radius').min(0.01).max(20).step(0.01).onFinishChange(generateGalaxy)
+// gui.add(parameters, 'branches').min(2).max(20).step(1).onFinishChange(generateGalaxy)
+// gui.add(parameters, 'randomness').min(0).max(2).step(0.001).onFinishChange(generateGalaxy)
+// gui.add(parameters, 'randomnessPower').min(1).max(10).step(0.001).onFinishChange(generateGalaxy)
+// gui.addColor(parameters, 'insideColor').onFinishChange(generateGalaxy)
+// gui.addColor(parameters, 'outsideColor').onFinishChange(generateGalaxy)
 
-const positions = new Float32Array(particleCount * 3);
-for (let i = 0; i < particleCount * 3; i++) {
-  positions[i] = (Math.random() - 0.5) * 30;
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
 }
 
-particleGeometry.setAttribute(
-  "position",
-  new THREE.BufferAttribute(positions, 3)
-);
-const particleMaterial = new THREE.PointsMaterial({
-  size: 0.2,
-  sizeAttenuation: true,
-  fog: false,
-  color: 0xf7f7ff,
-});
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
 
-const particlePoints = new THREE.Points(particleGeometry, particleMaterial);
-scene.add(particlePoints);
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
 
 /**
  * Camera
  */
-const camera = new THREE.PerspectiveCamera(
-  45,
-  sizes.width / sizes.height,
-  0.1,
-  500
-);
-camera.position.z = 20;
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 3
+camera.position.y = 3
+camera.position.z = 3
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
 
 /**
  * Renderer
  */
-const renderer = new THREE.WebGL1Renderer({
-  canvas: document.querySelector(".webgl"),
-  alpha: true,
-  antialias: true,
-});
-renderer.setSize(sizes.width, sizes.height);
-renderer.setPixelRatio(Math.min(devicePixelRatio, 2));
-renderer.physicallyCorrectLights = true;
-renderer.outputEncoding = THREE.sRGBEncoding;
-renderer.toneMapping = THREE.ACESFilmicToneMapping;
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setClearColor('#0d4690')
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 /**
- * Controls
+ * Generate the first galaxy
  */
-let controls = null;
-
-if (innerWidth <= 1024) {
-  addControls();
-} else {
-  camera.position.set(0, 0, 20);
-
-  addEventListener("mousemove", (event) => {
-    mouse.x = event.clientX / window.innerWidth - 0.5;
-    mouse.y = -(event.clientY / window.innerHeight - 0.5);
-  });
-}
-
-function addControls() {
-  camera.position.y = 4;
-  camera.position.x = 4;
-  camera.position.z = 40;
-
-  controls = new OrbitControls(camera, renderer.domElement);
-  controls.enablePan = false;
-  controls.enableDamping = true;
-  controls.minPolarAngle = (Math.PI * 1) / 3;
-  controls.maxPolarAngle = (Math.PI * 2) / 3;
-  controls.minDistance = 8;
-  controls.maxDistance = 45;
-
-  controls.addEventListener("change", () => {
-    renderer.render(scene, camera);
-  });
-}
-
-function disposeMobileControls() {
-  if (controls) controls.dispose();
-}
+generateGalaxy()
 
 /**
- * Update Canvas on Resize
+ * Animate
  */
-addEventListener("resize", () => {
-  sizes.height = innerHeight;
-  sizes.width = innerWidth;
+const clock = new THREE.Clock()
 
-  camera.aspect = sizes.width / sizes.height;
-  camera.updateProjectionMatrix();
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
 
-  renderer.setSize(sizes.width, sizes.height);
+    // Update material
+    material.uniforms.uTime.value = elapsedTime
 
-  if (innerWidth <= 1024) {
-    disposeMobileControls();
-    addControls();
-  } else {
-    disposeMobileControls();
-    camera.position.set(0, 0, 20);
+    // Update controls
+    controls.update()
 
-    addEventListener("mousemove", (event) => {
-      mouse.x = event.clientX / window.innerWidth - 0.5;
-      mouse.y = -(event.clientY / window.innerHeight - 0.5);
-    });
-  }
-});
+    // Render
+    renderer.render(scene, camera)
 
-const clock = new THREE.Clock();
-
-function animate() {
-  const elapsedTime = clock.getElapsedTime();
-
-  if (objectGroup !== null) {
-    objectGroup.rotation.y += Math.sin(elapsedTime) * 0.01;
-    objectGroup.rotation.x += Math.sin(elapsedTime) * 0.01;
-
-    camera.position.x += Math.sin(elapsedTime) * 3;
-    camera.position.y += Math.sin(-elapsedTime) * 3;
-    camera.position.z += Math.sin(elapsedTime) * 0.05;
-
-    particlePoints.rotation.y += Math.sin(elapsedTime) * 0.01;
-    particlePoints.rotation.x += Math.cos(elapsedTime) * 0.01;
-  }
-  if (innerWidth <= 1024) {
-    controls.update();
-  } else {
-    disposeMobileControls();
-
-    target.x = mouse.x * 50;
-    target.y = mouse.y * 50;
-
-    camera.position.x += 0.1 * (target.x - camera.position.x);
-    camera.position.y += 0.1 * (target.y - camera.position.y);
-    camera.lookAt(new THREE.Vector3());
-  }
-  renderer.render(scene, camera);
-  requestAnimationFrame(animate);
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
 }
+
+tick()
